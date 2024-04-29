ce9716d53a137bfb6778d874f9cd8a40cd822982
wdyntr <widyantoro0603@gmail.com>
2024-04-29 19:36:28 +0700

    push

---
+++ src/notes.js
@@ -0,0 +1,12 @@
+const notes = [
+    // {
+    //     id: "", // Gunakan tanda kutip untuk menandai bahwa ini adalah string kosong
+    //     title: "", // Juga gunakan tanda kutip untuk string kosong
+    //     createdAt: "", // String kosong untuk createdAt
+    //     updatedAt: "", // String kosong untuk updatedAt
+    //     tags: [], // Gunakan tanda kurung siku untuk mendefinisikan array kosong
+    //     body: "", // String kosong untuk body
+    // },
+];
+
+module.exports = notes;
